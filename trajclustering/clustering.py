7d2ad25827403e1a5e1a0b592626583572e54486
Kelsie King <kelsieking23@vt.edu>
2024-06-25 13:24:49 -0700

    Added clustering.py

---
+++ trajclustering/clustering.py
@@ -0,0 +1,36 @@
+import MDAnalysis as mda
+from MDAnalysis.analysis.base import AnalysisBase
+import numpy as np
+
+class GROMOS(AnalysisBase):
+    def __init__(self, atomgroup, **kwargs):
+        self.atomgroup = atomgroup
+        super().__init__(atomgroup.universe.trajectory,
+                         **kwargs)
+    def _prepare(self):
+        # called before iteration on the trajectory has begun.
+        # initialize results
+        # =====================================================
+        # here, probably make an array that assigns frames to a 
+        # cluster 
+        # OR probably 
+        pass
+
+    def _single_frame(self):
+        # called after the trajectory is moved onto each new frame.
+        pass
+    
+    def _conclude(self):
+        # put everything together
+        pass
+    
+    def _rmsd_to_other_frames(self):
+        pass
+    
+    def _neighbor_count(self):
+        pass
+    
+    def _compute_rmsd(self):
+        # actually compute rmsd 
+        pass
+        
